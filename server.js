2bdec532d184d24603f512a61f2edcd76788ac4a
Rahul Yadav <ryrahul12345@gmail.com>
2023-06-21 13:58:13 +0530

    used it with express js

--- server.js
+++ server.js
@@ -1,53 +1,56 @@
-const http = require("http");
+const express = require('express');
+
+// Initialize the app 
+const app = express();
+// Application will now use JSON format for data
+app.use(express.json());
 
 const port = 8081;  //local port number
 
-const toDoList = ["complete Node byte", "complete React byte", "complete Mongo byte"]
+const toDoList = ["complete Node byte", "Play Cricket"];
 
-http.createServer((request, response) =>{ //callback function
-
-    // response.writeHead(200, {'Content-Type': 'text/html'});
-    // response.write("Hello World");
-    
-    const {method,url} = request;
-    console.log(method, url);
-
-    if(url==="/todos"){
-        if(method==="GET"){
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(toDoList.toString());
-        }
-// asdf
-        else if(method==="POST"){
-            let body="";
-            request.on("error", (err) => {
-                console.error(err);
-            }).on("data", (chunk) => {
-                body += chunk;
-            })
-        
-        }
-
-        else{ 
-            response.writeHead(501, {'Content-Type': 'text/html'});
-            response.write("404 Not Found");
-        }
-    }else{
-        response.writeHead(404, {'Content-Type': 'text/html'});
-        response.write("404 Not Found");
-    }
-    
-    response.end();
-
-}).listen(port, () => { //need to tell him where to create a server at port 8081  , callback function
-
-    console.log(`Server running at port ${port}`);
+// http://localhost:8081/todos
+app.get("/todos",(req,res)=>{
+    res.status(200).send(toDoList);
 });
 
+// http://localhost:8081/todos
+app.post("/todos",(req,res)=>{
+    let newToDoItem = req.body.Item;
+    toDoList.push(newToDoItem);
+    res.status(201).send({
+        message: "Item added successfully"
+    });
 
+});
 
-// http://localhost:8081
+// http://localhost:8081/todos
+app.delete("/todos",(req,res)=>{
+    let itemToDelete = req.body.Item;
 
-// npm start - to start the server
-// node server.js - to start the server
-// npm run runthis - to start the server
+    toDoList.find((item,index)=>{
+        if(item===itemToDelete){
+            toDoList.splice(index,1);
+        }
+    });
+
+    res.status(200).send({
+        message:`Item ${itemToDelete} deleted successfully`
+    });
+
+});
+
+// check for this route if any method apart from above one of todos then give this error
+app.all("/todos",(req,res)=>{
+   res.status(501).send();
+});
+
+// if try to use any other route apart from here then give error
+app.all("*",(req,res)=>{
+    res.status(501).send();
+});
+
+app.listen(port,()=>{
+    console.log(`Server listening on port ${port}`);
+});
+
